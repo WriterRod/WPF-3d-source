2f5279a2221e5da77cc5d18d5b6db745a88cb126
Macrotitech <Macrotitech@outlook.com>
2020-05-11 21:21:07 +0200

    Changed Ch33\LoadObj to use InvariantCulture, so that obj files can be parsed also on systems using "," as decimal separator.

--- Ch33/LoadObj/App.xaml.cs
+++ Ch33/LoadObj/App.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 
 namespace LoadObj
@@ -13,5 +15,12 @@ namespace LoadObj
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.DefaultThreadCurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+        }
+       
+        
     }
 }
